a708362a52d894d0d80d782f62aa85dc5f411c5d
achatto4 <149892725+achatto4@users.noreply.github.com>
2023-12-07 02:56:59 -0500

    Delete src directory

--- src/Common/Verbose.cpp
+++
@@ -1,5 +0,0 @@
-#include"Verbose.h"
-
-#ifdef VERBOSE_DYN
-bool verbose_mode=true;
-#endif
--- src/OT_SparseSimplex/sparsebasicfeasible.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPARSEBASICFEASIBLE_H_
-#define SPARSEBASICFEASIBLE_H_
-
-void dedewithchannels(int m, int n, int no_of_ones, int *basis, int *channels_byrow_over, int **channels_byrow);
-void findblocks(int m, int n, int *basis, int *nblock, int *rowblock, int *colblock);
-
-#endif
-
