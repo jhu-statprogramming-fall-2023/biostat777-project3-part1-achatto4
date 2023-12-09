dbed50be39ee6ca30dc571160ab18a6ff87be340
achatto4 <149892725+achatto4@users.noreply.github.com>
2023-12-09 03:36:58 -0500

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,25 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+library(pkgdown)
+library(usethis)
+
+# Run once to configure package to use pkgdown
+#usethis::use_pkgdown()
+# Run to build the website
+pkgdown::build_site()
+
+#use_vignette("Example_analysis", "Example analysis")
+#use_readme_rmd()
